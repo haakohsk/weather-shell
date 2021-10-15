e21ecbbc02eb4c7eb53c5d3f56f29cc940addc23
erlingcf <erlingcf@ntnu.stud.no>
2021-10-15 19:06:12 +0200

    Structured program into tasks

--- weather-shell.sh
+++ weather-shell.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-echo "Check out todays weather for $(location)"
+
+# Fetch user location
+
+# Handle user location
+
+# Fetch weatherforecast from API
+
+# Store JSON data from API to variable
+
+# Handle JSON data with JQ
+
+# Output weather forecast to user stdout
