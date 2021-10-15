a8341cdd3a67b92a695e057c13a2034ad0926001
erlingcf <erlingcf@ntnu.stud.no>
2021-10-15 19:48:26 +0200

    Started fetching weatherdata from API

--- weather-shell.sh
+++ weather-shell.sh
@@ -2,9 +2,17 @@
 
 # Fetch user location
 
+# Get address from coordinates
+
 # Handle user location
 
 # Fetch weatherforecast from API
+forecastJSON=$(curl --request GET \
+    --url 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=63.4260679&lon=10.4065994&altitude=58')
+
+# forecastJSON.properties.timeseries.next_12_hours
+# echo $forecast
+jq -r --arg forecastJSON '."$forecastJSON"'
 
 # Store JSON data from API to variable
 
