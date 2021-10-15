bd9e89c15165f68ed0feb62208adc86d3f867257
Håkon Skaufel <hakonskaufel@hakons-macbook-pro.home>
2021-10-15 21:41:45 +0200

    Update script

--- weather-shell.sh
+++ weather-shell.sh
@@ -11,11 +11,13 @@ forecastJSON=$(curl --request GET \
     --url 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=63.4260679&lon=10.4065994&altitude=58')
 
 # forecastJSON.properties.timeseries.next_12_hours
-# echo $forecast
-jq -r --arg forecastJSON '."$forecastJSON"'
+# echo $forecastJSON | jq '.[1]'
+# jq -r --arg forecastJSON '."$forecastJSON"'
 
 # Store JSON data from API to variable
 
 # Handle JSON data with JQ
 
 # Output weather forecast to user stdout
+
+echo $forecastJSON
