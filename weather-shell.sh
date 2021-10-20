ab41779e2b7201997cd9025f9871d329ffef5544
Håkon Skaufel <hakonskaufel@dhcp-10-22-1-65.wlan.ntnu.no>
2021-10-20 16:19:48 +0200

    Update weather-shell.sh

--- weather-shell.sh
+++ weather-shell.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-# Fetch user location
+# Fetch user location(PENDING)
 
-# Get address from coordinates
-
-# Handle user location
+# Handle user location(PENDING)
 
 # Fetch weatherforecast from API
-forecastJSON=$(curl --request GET \
-    --url 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=63.4260679&lon=10.4065994&altitude=58')
-
-# forecastJSON.properties.timeseries.next_12_hours
-# echo $forecastJSON | jq '.[1]'
-# jq -r --arg forecastJSON '."$forecastJSON"'
+forecastJSON=$(curl --silent --request GET \
+    --url 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=63.4260679&lon=10.4065994&altitude=58');
+now=$(date);
 
 # Store JSON data from API to variable
 
 # Handle JSON data with JQ
 
-# Output weather forecast to user stdout
+# Print weather info
+echo ------------------------------------------;
+echo "Today is: $now";
+echo --- Weather at the moment ---;
+echo Temperature in celsius:;
+echo $forecastJSON | jq -r '.properties.timeseries[0].data.instant.details.air_temperature';
+echo Wind speed in m/s:;
+echo $forecastJSON | jq -r '.properties.timeseries[0].data.instant.details.wind_speed';
 
-echo $forecastJSON
+echo;
+echo;
+echo -- Weather for next 6 hours --;
+echo General info:;
+echo $forecastJSON | jq -r '.properties.timeseries[0].data.next_6_hours.summary.symbol_code';
+
+echo Precipitation in mm:
+echo $forecastJSON | jq -r '.properties.timeseries[0].data.next_6_hours.details.precipitation_amount';
+echo ------------------------------------------;
