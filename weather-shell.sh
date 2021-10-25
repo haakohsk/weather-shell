5d52680629a395f60985321b469184a39eb60d47
Erling Fladvad <80626436+ZigmaF@users.noreply.github.com>
2021-10-25 22:23:02 +0200

    Update weather-shell.sh
    Placename calls to API works and extracted coordinates are sent as parameter to weather API for fetching

--- weather-shell.sh
+++ weather-shell.sh
@@ -6,58 +6,63 @@ now=$(date);
 # Keys
 positionstack_api_key="YOUR_API_KEY"
 
-# Fetch user location(PENDING)
+# Function that prints weather to stdout, takes in JSON data
+function printWeather {
+    JSONdata=$1
 
-# Handle user location(PENDING)
+    echo ------------------------------------------;
+    echo "Today is: $now";
+    echo --- Weather at the moment ---;
+    echo Temperature in celsius:;
+    echo $JSONdata | jq -r '.properties.timeseries[0].data.instant.details.air_temperature';
+    echo Wind speed in m/s:;
+    echo $JSONdata | jq -r '.properties.timeseries[0].data.instant.details.wind_speed';
+
+    echo;
+    echo;
+    echo -- Weather for next 6 hours --;
+    echo General info:;
+    echo $JSONdata | jq -r '.properties.timeseries[0].data.next_6_hours.summary.symbol_code';
+
+    echo Precipitation in mm:
+    echo $JSONdata | jq -r '.properties.timeseries[0].data.next_6_hours.details.precipitation_amount';
+    echo ------------------------------------------;
+}
+
+# Function that takes in lat and lon and makes call to weather api
+ function callWeatherAPI {
+     lat=$1
+     lon=$2
+
+     # Fetch weatherdata for location
+     forecastJSON=$(curl --silent --request GET \
+          --url "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${lon}")
+
+     # Functioncall with data for location as parameter
+     printWeather $forecastJSON
+ }
 
 # If user specifies placename
 # (Now invoked if a argument is passed)
 if [ $# -eq 1 ]
   then
     placenameInput=$1
-    echo $placenameInput
+
     # Fetch coordinates of placename
     placenameJSON=$(curl --silent --request GET \
         --url  "http://api.positionstack.com/v1/forward?access_key=${positionstack_api_key}&query=${placenameInput}")
-    echo $placenameJSON
 
-    #http://api.positionstack.com/v1/forward?        access_key=0879fbc3773607fe1440b8ab2f802983&query=Trondheim
-     # Handle JSON data with JQ
-     # coords_placename=$(echo $forecastJSON | jq -r ".data[].continent")
+    # Extract latitude and longitude and store in variables
+     coords_placename_lat=$(echo $placenameJSON | jq -r ".data[].latitude")
+     coords_placename_lng=$(echo $placenameJSON | jq -r ".data[].longitude")
 
-     oords_placename=$(echo $forecastJSON | jq -r '.data[] | {latitude}' )
-     echo $coords_placename
-# jq '.results[] | {name, age}'
+    # Functioncall with coords as parameters to function
+    callWeatherAPI $coords_placename_lat $coords_placename_lng
 fi
 
-# Fetch weatherforecast from API
 # if called with no argument
 if [ $# -eq 0 ]
   then
     # Use deafult coords, Nedre Petersborg gate 2
-    forecastJSON=$(curl --silent --request GET \
-        --url 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=63.4260679&lon=10.4065994&altitude=58');
-
-# Store JSON data from API to variable
-
-# Handle JSON data with JQ
-
-# Print weather info
-echo ------------------------------------------;
-say "Today is: $now";
-echo --- Weather at the moment ---;
-say Temperature in celsius:;
-say $forecastJSON | jq -r '.properties.timeseries[0].data.instant.details.air_temperature';
-echo Wind speed in m/s:;
-echo $forecastJSON | jq -r '.properties.timeseries[0].data.instant.details.wind_speed';
-
-echo;
-echo;
-echo -- Weather for next 6 hours --;
-echo General info:;
-echo $forecastJSON | jq -r '.properties.timeseries[0].data.next_6_hours.summary.symbol_code';
-
-echo Precipitation in mm:
-echo $forecastJSON | jq -r '.properties.timeseries[0].data.next_6_hours.details.precipitation_amount';
-echo ------------------------------------------;
+    callWeatherAPI 63.4260679 10
 fi
